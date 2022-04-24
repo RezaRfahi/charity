61e9fc77d51ef2ad6416c4bac7f83ac048a9e4b1
RezaRfahi <refahireza82@gmail.com>
2022-04-24 09:53:09 +0430

    set paymanet migration field

--- database/migrations/2022_04_24_051232_create_payments_table.php
+++ database/migrations/2022_04_24_051232_create_payments_table.php
@@ -15,6 +15,8 @@ class CreatePaymentsTable extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->text('serial');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
