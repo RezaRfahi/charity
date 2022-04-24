601cd9baf93af16589a777100428f34c19169f25
RezaRfahi <refahireza82@gmail.com>
2022-04-24 10:09:39 +0430

    paymanets model relationship to user

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    public function user(){
+        $this->belongsTo(User::class,'user_id','id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function payments(){
+        return $this->hasMany(Payment::class);
+    }
 }
