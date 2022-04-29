c0640bc14736c50d09eb126aa5019f4f5e802e34
RezaRfahi <refahireza82@gmail.com>
2022-04-29 15:12:44 +0430

    modified permission_user migration add forigen and unique fields

--- database/migrations/2022_04_24_062408_create_permission_user_table.php
+++ database/migrations/2022_04_24_062408_create_permission_user_table.php
@@ -15,7 +15,11 @@ class CreatePermissionUserTable extends Migration
     {
         Schema::create('permission_user', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->unsignedBigInteger('permission_id');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+
+            $table->unique(['user_id','permission_id']);
         });
     }
 
