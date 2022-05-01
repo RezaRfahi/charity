642a1a3b4f7b46f4b5c6b83a90529258bde4ae4d
RezaRfahi <refahireza82@gmail.com>
2022-05-01 11:21:32 +0430

    create admin prefix index Route:get

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +26,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::prefix('admin')->group(function(){
+    Route::get('/',[AdminController::class,'index'])->name('admin/index');
 });
