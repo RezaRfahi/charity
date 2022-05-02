b4c2f7cf036eced99c400ca56d7e3d9971ee1602
RezaRfahi <refahireza82@gmail.com>
2022-05-02 09:27:00 +0430

    set confirm_password page language to fa

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -4,8 +4,8 @@
             <x-jet-authentication-card-logo />
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+        <div dir="rtl" class="mb-4 text-sm text-gray-600">
+            {{ __('این یک منطقه امن برنامه است. لطفاً قبل از ادامه رمز عبور خود را تأیید کنید.') }}
         </div>
 
         <x-jet-validation-errors class="mb-4" />
@@ -14,13 +14,13 @@
             @csrf
 
             <div>
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="{{ __('رمزعبور') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" autofocus />
             </div>
 
             <div class="flex justify-end mt-4">
                 <x-jet-button class="ml-4">
-                    {{ __('Confirm') }}
+                    {{ __('تایید') }}
                 </x-jet-button>
             </div>
         </form>
