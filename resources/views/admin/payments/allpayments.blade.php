8175de5aecb0fcb537373174d48f187e8dbb5c04
RezaRfahi <refahireza82@gmail.com>
2022-05-07 01:04:56 +0430

    modify all payments, set static table

--- resources/views/admin/payments/allpayments.blade.php
+++ resources/views/admin/payments/allpayments.blade.php
@@ -1,3 +1,72 @@
 @extends('admin.master.masterpage')
 
+@section('Content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="callout callout-info">
+              <h5><i class="fa fa-info"></i> نکته :</h5>
+              این صفحه مناسب برای پرینت طراحی شده است
+            </div>
 
+
+            <!-- Main content -->
+            <div class="invoice p-3 mb-3">
+              <!-- title row -->
+              <div class="row">
+                <div class="col-12">
+                  <h4>
+                    <i class="fa fa-globe"></i>سامانه خیریه شمسی پور
+                  </h4>
+                </div>
+                <!-- /.col -->
+              </div>
+
+              <!-- Table row -->
+              <div class="row">
+                <div class="col-12 table-responsive">
+                  <table class="table table-striped">
+                    <thead>
+                    <tr>
+                      <th>نام پرداخت کننده</th>
+                      <th>سریال پرداخت</th>
+                      <th>مبلغ</th>
+                      <th>وضعیت</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                      <td>رضا رفاهی</td>
+                      <td>455-981-221</td>
+                      <td>150000 تومان</td>
+                      <td style="color: green">موفق</td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+
+              <!-- this row will not appear when printing -->
+              <div class="row no-print">
+                <div class="col-12">
+                  <a href="invoice-print.html" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> پرینت </a>
+                  </div>
+              </div>
+            </div>
+            <!-- /.invoice -->
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+@endsection
