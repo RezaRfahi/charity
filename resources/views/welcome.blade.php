a657eeca275fea2a00ae7df37923019a0da0ff41
RezaRfahi <refahireza82@gmail.com>
2022-05-07 00:30:32 +0430

    add logo in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div dir="rtl" class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    //logo source
+                   <img src="{{asset('img/sitelogo.png')}}" alt="">
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
